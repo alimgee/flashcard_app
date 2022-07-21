963b87a41d8ee1c339c523978ad1b167e1f91746
alanmcgee <mcgee.al@gmail.com>
2022-07-21 19:36:01 +0100

    Adding edit views and links from list template

--- cards/views.py
+++ cards/views.py
@@ -3,6 +3,7 @@ from django.urls import reverse_lazy
 from django.views.generic import (
     ListView,
     CreateView,
+    UpdateView,
 )
 
 from .models import Card
@@ -15,3 +16,6 @@ class CardCreateView(CreateView):
     model = Card
     fields = ["question", "answer", "box"]
     success_url = reverse_lazy("card-create")
+
+class CardUpdateView(CardCreateView, UpdateView):
+    success_url = reverse_lazy("card-list")
